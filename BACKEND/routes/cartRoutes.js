ce3bfd32fff018a0364d31543c5c08628ba49086
Shreyaan <shreyaan.l23@iiits.in>
2026-04-19 16:10:47 +0530

    redis cart

--- BACKEND/routes/cartRoutes.js
+++ BACKEND/routes/cartRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const cartController = require("../controller/cartController");
 const {
 	verify,
-	cacheResponse,
 	cacheTags,
 	invalidateTagsOnSuccess,
 } = require("../middleware");
@@ -13,40 +12,18 @@ const {
 router.use(verify);
 
 // Cart routes
-router.get(
-	"/",
-	cacheResponse({
-		ttlSeconds: 90,
-		namespace: "cart",
-		varyByUser: true,
-		tags: (req) => [cacheTags.cart(req.user?._id)],
-	}),
-	cartController.getCart,
-);
+router.get("/", cartController.getCart);
 
-router.post(
-	"/add/:courseId",
-	invalidateTagsOnSuccess((req) => [cacheTags.cart(req.user?._id)]),
-	cartController.addToCart,
-);
+router.post("/add/:courseId", cartController.addToCart);
 
-router.delete(
-	"/remove/:courseId",
-	invalidateTagsOnSuccess((req) => [cacheTags.cart(req.user?._id)]),
-	cartController.removeFromCart,
-);
+router.delete("/remove/:courseId", cartController.removeFromCart);
 
-router.delete(
-	"/clear",
-	invalidateTagsOnSuccess((req) => [cacheTags.cart(req.user?._id)]),
-	cartController.clearCart,
-);
+router.delete("/clear", cartController.clearCart);
 
 router.post("/create-payment-order", cartController.createRazorpayOrder);
 router.post(
 	"/verify-payment",
 	invalidateTagsOnSuccess((req) => [
-		cacheTags.cart(req.user?._id),
 		cacheTags.student(req.user?._id),
 		cacheTags.coursesPublic,
 		cacheTags.statsPublic,
@@ -62,7 +39,6 @@ router.post(
 router.put(
 	"/update-enroll-courses",
 	invalidateTagsOnSuccess((req) => [
-		cacheTags.cart(req.user?._id),
 		cacheTags.student(req.user?._id),
 		cacheTags.coursesPublic,
 		cacheTags.statsPublic,
