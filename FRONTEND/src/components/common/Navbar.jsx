48ae14df2f7559dbc1042aea2b4425ba51d8d8ef
Aryan Choudhary <aryanchoudhary3232@gmail.com>
2026-04-19 14:27:35 +0530

    testing

--- FRONTEND/src/components/common/Navbar.jsx
+++ FRONTEND/src/components/common/Navbar.jsx
@@ -114,7 +114,7 @@ const Navbar = () => {
           className="brand"
           onClick={closeMenu}
         >
-          SeekhoBharat
+          SeekhoBharat is very good
         </Link>
 
         {/* Hamburger Icon (Visible on Mobile) */}
